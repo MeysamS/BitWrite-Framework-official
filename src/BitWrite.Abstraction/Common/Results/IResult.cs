5e99710c1bfea82f658fecfc1e612cd7fb9f60f0
meysams <soleymani.meysam.developer@gmail.com>
2025-04-17 01:50:13 +0330

    declare Result base class in common folder on abstraction layer

---
+++ src/BitWrite.Abstraction/Common/Results/IResult.cs
@@ -0,0 +1,29 @@
+﻿namespace BitWrite.Abstraction.Common.Results;
+
+/// <summary>
+/// Represents a result of an operation
+/// </summary>
+public interface IResult
+{
+    /// <summary>
+    /// Gets a value indicating whether the operation was successful
+    /// </summary>
+    bool IsSuccess { get; }
+
+    /// <summary>
+    /// Gets the error message if the operation failed
+    /// </summary>
+    string? ErrorMessage { get; }
+}
+
+/// <summary>
+/// Represents a result of an operation with data
+/// </summary>
+/// <typeparam name="T">Type of the result data</typeparam>
+public interface IResult<out T> : IResult
+{
+    /// <summary>
+    /// Gets the result data
+    /// </summary>
+    T? Data { get; }
+}
---
+++ src/BitWrite.Abstraction/Common/Results/ResultBase.cs
@@ -0,0 +1,51 @@
+﻿namespace BitWrite.Abstraction.Common.Results;
+
+/// <summary>
+/// Represents a base class for results
+/// </summary>
+public abstract class ResultBase : IResult
+{
+    /// <summary>
+    /// Gets a value indicating whether the result is successful
+    /// </summary>
+    public bool IsSuccess { get; }
+
+    /// <summary>
+    /// Gets the error message
+    /// </summary>
+    public string? ErrorMessage { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the ResultBase class
+    /// </summary>
+    /// <param name="success">A value indicating whether the result is successful</param>
+    /// <param name="errorMessage">The error message</param>
+    protected ResultBase(bool success, string? errorMessage)
+    {
+        IsSuccess = success;
+        ErrorMessage = errorMessage;
+    }
+}
+
+/// <summary>
+/// Represents a base class for results with a generic type
+/// </summary>
+/// <typeparam name="T">The type of data</typeparam>
+public abstract class ResultBase<T> : ResultBase, IResult<T>
+{
+    /// <summary>
+    /// Initializes a new instance of the ResultBase class
+    /// </summary>
+    /// <param name="success">A value indicating whether the result is successful</param>
+    /// <param name="errorMessage">The error message</param>
+    /// <param name="data">The data</param>
+    protected ResultBase(bool success, string? errorMessage, T data) : base(success, errorMessage)
+    {
+        Data = data;
+    }
+
+    /// <summary>
+    /// Gets the data
+    /// </summary>
+    public T Data { get; }
+}
