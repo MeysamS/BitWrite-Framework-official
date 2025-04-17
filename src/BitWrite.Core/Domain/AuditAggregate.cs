ac6720139eb14536e614db7e12ad1a16c2ff400b
meysams <soleymani.meysam.developer@gmail.com>
2025-04-17 03:48:09 +0330

    declration audit class for Aggregate and Entity in core layer

---
+++ src/BitWrite.Core/Domain/AuditAggregate.cs
@@ -0,0 +1,25 @@
+﻿using BitWrite.Abstraction.Domain.Auditable;
+using BitWrite.Abstraction.Domain.Identity;
+
+namespace BitWrite.Core.Domain;
+
+public abstract class AuditAggregate<TId> : AggregateRoot<TId>, IAuditableEntity<TId>
+    where TId : notnull
+{
+
+    public DateTime? UpdatedDate { get; protected set; } = default!;
+
+    public int? UpdatorId { get; protected set; } = default!;
+
+}
+
+
+public abstract class AuditAggregate<TIdentity, TId> : AuditAggregate<TIdentity>
+    where TIdentity : Identity<TId>
+{
+}
+
+public abstract class AuditAggregate : AuditAggregate<Identity<long>, long>
+{
+}
+
---
+++ src/BitWrite.Core/Domain/AuditableEntity.cs
@@ -0,0 +1,23 @@
+﻿using BitWrite.Abstraction.Domain.Auditable;
+using BitWrite.Abstraction.Domain.Identity;
+
+namespace BitWrite.Core.Domain;
+
+public class AuditableEntity<TId> : Entity<TId>, IAuditableEntity<TId>
+    where TId : notnull
+{
+
+    public DateTime? UpdatedDate { get; protected set; }
+
+    public int? UpdatorId { get; protected set; }
+
+}
+
+public abstract class AuditableEntity<TIdentity, TId> : AuditableEntity<TIdentity>
+where TIdentity : Identity<TId>
+{
+}
+
+public class AuditableEntity : AuditableEntity<Identity<long>, long>
+{
+}
